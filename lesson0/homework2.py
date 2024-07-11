bd345449a418dcd9a6d56ac88db3c566640c6375
BelArtCom <belartcom2009@gmail.com>
2024-07-11 16:18:04 +1000

    Add files via upload

---
+++ lesson0/homework2.py
@@ -0,0 +1,7 @@
+homework_sum = 12
+hours_sum = 1.5
+courses_name = 'Python'
+exercise_time = hours_sum / homework_sum
+
+print('Курс:',courses_name,', всего задач:',homework_sum,', затрачено часов:',hours_sum,', среднее время выполнения:',exercise_time,'часа.')
+print('Курс: ' + courses_name + ', всего задач: ' + str(homework_sum) + ', затрачено часов: ' + str(hours_sum) + ', среднее время выполнения: ' + str(exercise_time) + ' часа.')
