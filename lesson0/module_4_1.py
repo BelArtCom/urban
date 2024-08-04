ae812ecaf483ed9cdca4f95f3cd11f73f0926371
BelArtCom <belartcom2009@gmail.com>
2024-08-04 22:32:27 +1000

    Update module_4_1.py

--- lesson0/module_4_1.py
+++ lesson0/module_4_1.py
@@ -1,10 +1,14 @@
+# Импортирую функцию divide из модуля fake_math и присваиваю ей псевдоним fake_divide
 from fake_math import divide as fake_divide
+
+# Импортирую функцию divide из модуля true_math и присваиваю ей псевдоним true_divide
 from true_math import divide as true_divide
 
 result1 = fake_divide(69, 3)
 result2 = fake_divide(3, 0)
 result3 = true_divide(49, 7)
 result4 = true_divide(15, 0)
+
 print(result1)
 print(result2)
 print(result3)
