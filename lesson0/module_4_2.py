7c3d061e020c7ddb08a39649753fb56ba28b61a3
BelArtCom <belartcom2009@gmail.com>
2024-08-12 22:03:18 +1000

    Update module_4_2.py

--- lesson0/module_4_2.py
+++ lesson0/module_4_2.py
@@ -4,11 +4,12 @@ def test_function():
         print('START inner_function')
         print('Я в области видимости функции test_function')
     inner_function()
+
+test_function()
+
 #inner_function()
 # При вызове внутренней функции inner_function вне функции test_function выходит ошибка
 # name 'inner_function' is not defined
 # Система не видит определения функции
 # Python не понимает, откуда взялась функция inner_function и не знает, как с ней работать
 # Функция inner_function находится вне области видимости
-
-test_function()
