1207b6109b3af6b90ac1457d87fb77a6286c5e3c
BelArtCom <belartcom2009@gmail.com>
2024-09-20 12:06:44 +1000

    Update module_6_3.py

--- lesson0/module_6_3.py
+++ lesson0/module_6_3.py
@@ -1,7 +1,6 @@
 # Родительский класс Horse, описывающий лошадь
 class Horse:
     def __init__(self):
-        print('INIT HORSE')
         self.x_distance = 0 # пройденный путь
         self.sound = 'Frrr' #звук, который издаёт лошадь
 
@@ -12,7 +11,6 @@ class Horse:
 # Родительский класс Eagle, описывающий орла
 class Eagle:
     def __init__(self):
-        print('INIT EAGLE')
         self.y_distance = 0 # высота полёта
         self.sound = 'I train, eat, sleep, and repeat' # звук, который издаёт орёл (отсылка)
 
@@ -24,18 +22,13 @@ class Eagle:
 class Pegasus(Horse, Eagle):
 # Объект такого класса должен обладать атрибутами классов родителей в порядке наследования
     def __init__(self):
-        print('INIT PEGASUS')
-        self.x_distance = Horse().x_distance
-        self.y_distance = Eagle().y_distance
-        self.sound = Horse().sound
-        self.sound = Eagle().sound
+        Horse.__init__(self)
+        Eagle.__init__(self)
 
 # Метод move вызывает методы run и fly родительских классов
     def move(self, dx, dy):
-        self.dx = dx
-        self.dy = dy
-        super().run(self.dx)
-        super().fly(self.dy)
+        self.run(dx)
+        self.fly(dy)
 
 # Метод get_pos возвращает текущее положение пегаса в виде кортежа - (x_distance, y_distance)
     def get_pos(self):
@@ -45,15 +38,7 @@ class Pegasus(Horse, Eagle):
     def voice(self):
         print(self.sound)
 
-print('Цепочка наследования MRO ----------------------------------------------')
-print('Класс   Horse: ', Horse.mro())
-print('Класс   Eagle: ', Eagle.mro())
-print('Класс Pegasus: ', Pegasus.mro())
-print('-----------------------------------------------------------------------')
-
-print('Создание экземпляра класса Pegasus')
 p1 = Pegasus()
-print('Результат -------------------------------------------------------------')
 print(p1.get_pos())
 p1.move(10, 15)
 print(p1.get_pos())
