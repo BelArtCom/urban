65560f934270223a2b8b820a4975492cc8d44b84
BelArtCom <belartcom2009@gmail.com>
2024-11-23 16:41:03 +1000

    Add files via upload

---
+++ lesson0/crud_functions.py
@@ -0,0 +1,84 @@
+import sqlite3
+import random
+
+
+# Генератор 8-значного номера id
+def gen_id():
+    id: str = ''
+    for i in range(1, 8):
+        id += str(random.randint(1, 9))
+    return int(id)
+
+
+def create_db():
+    # Подключение к БД
+    connection = sqlite3.connect('not_telegram.db')
+    cursor = connection.cursor()
+
+    # Создание таблицы Products
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Products(
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT,
+    price INTEGER NOT NULL,
+    image TEXT NOT NULL
+    );
+                   ''')
+
+    # Проверка заполненности таблицы
+    cursor.execute('SELECT count(*) FROM Products')
+    total_products = cursor.fetchone()[0]
+
+    # Отключение от БД
+    connection.commit()
+    connection.close()
+
+    return total_products
+
+def insert_data():
+    # Подключение к БД
+    connection = sqlite3.connect('not_telegram.db')
+    cursor = connection.cursor()
+
+    # Заполнение таблицы Products данными
+    for i in range(4):
+        products = [
+            ['Витамин C', 'Антиоксидант', 'Vit_C.webp'],
+            ['Витамин D3', 'Обмен кальция и фосфора', 'Vit_D.webp'],
+            ['Витамин E', 'Замедляет старение клеток', 'Vit_E.webp'],
+            ['Витамин Омега-3', 'Здоровье сердца и сосудов', 'Vit_Omega.webp']
+        ]
+        cursor.execute('INSERT INTO Products (id, title, description, price, image) VALUES (?, ?, ?, ?, ?)',
+                       (f'{gen_id()}', f'{products[i][0]}', f'{products[i][1]}', f'{(i + 1) * 100}',
+                        f'{products[i][2]}'))
+    # Отключение от БД
+    connection.commit()
+    connection.close()
+
+
+def initiate_db():
+    create_db()
+    # Если таблица Products пуста, заполняем её данными
+    if not create_db():
+        insert_data()
+
+
+def get_all_products():
+
+    # Создание и заполнение БД
+    initiate_db()
+
+    # Подключение к БД
+    connection = sqlite3.connect('not_telegram.db')
+    cursor = connection.cursor()
+
+    # Выборка данных из таблицы Products
+    cursor.execute('SELECT * FROM Products ORDER BY id')
+    products = cursor.fetchall()
+
+    # Отключение от БД
+    connection.commit()
+    connection.close()
+
+    return products
